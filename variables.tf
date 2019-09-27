28e1e875b001bbc509436103314ac739d834bb98
Thota Bala Murali <Balamuralikrishna.thota@thomsonreuters.com>
2019-09-27 09:10:05 +0530

    FirstCommit

---
+++ variables.tf
@@ -0,0 +1,54 @@
+variable "aws_access_key" {
+  description = "Aws Account Access Key"
+  }
+variable "aws_secret_key" {
+    description = "Aws Account Secret Key"
+}
+variable "aws_region" {
+   description = "EC2 Region for the VPC"
+   default     =   "us-east-1"
+}
+
+variable "vpc_cidr" {
+      description = "VPC CIDR"
+      default = "10.0.0.0/16"
+  
+}
+variable "public_subnet_cidr" {
+    description = "Public Subnet - CIDR"
+    default = "10.0.1.0/24"
+  
+}
+variable "private_subnet_cidr" {
+    description = "Private Subnet - CIDR"
+    default  = "10.0.2.0/24"
+  
+}
+variable "ami" {
+    description ="EC2 AMI"
+    default =""
+  
+}
+variable "ami_win" {
+  description = "EC2 AMI"
+  default = "ami-085ea1972627f58fd"
+}
+variable "key_name" {
+  description = "AWS SSH Key Name"
+  default = "devopsone"
+}
+
+variable "DnsZoneName" {
+  default = "DevOpsOne.internal"
+  description = "DevOpsOne - internal dns Zone"
+}
+
+variable "instance_username" { 
+  default = "ec2-user" 
+} 
+
+variable "instance_password" { }
+
+
+
+
